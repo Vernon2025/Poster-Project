34b7d9d3b0f9126cbaf7aa68b362b11a36923b77
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-12-13 11:04:09 -0800

    First

---
+++ Battle.java
@@ -0,0 +1,40 @@
+public class Battle {
+  private boolean hasInfantry;
+  private boolean hasPlane;
+  private boolean hasTank;
+  private boolean attack;
+  private boolean defend;
+  private String hasArmy;
+  public void getBattleType() {
+      hasArmy = "We all fight!";
+      if (hasInfantry && hasTank && hasPlane == true) {
+          System.out.println(hasArmy);
+      } else if (hasInfantry == true) {
+          System.out.println("INFANTRY BATTLE");
+      } else if (hasPlane == true) {
+          System.out.println("AIR BATTLE");
+      } else if (hasTank == true) { 
+          System.out.println("TANK BATTLE");
+      }   
+    }
+  public void allOutWar() {
+      hasInfantry = true;
+      hasPlane= true;
+      hasTank = true;
+  }
+  public void infantryBattle() {
+      hasInfantry = true;
+      hasPlane= false;
+      hasTank = false;
+  }
+  public void planeBattle() {
+      hasInfantry = false;
+      hasPlane= true;
+      hasTank = false;
+  }
+  public void tankBattle() {
+      hasInfantry = false;
+      hasPlane= false;
+      hasTank = true;
+  }
+}
