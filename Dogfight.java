e5abecfc7ddc69c4bf82639fc7c3782edb6ec371
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-12-14 19:49:55 -0800

    Sixth

--- Dogfight.java
+++ Dogfight.java
@@ -1,17 +1,17 @@
 public class Dogfight extends Battle {
-    private boolean isfighting;
+    private boolean isFighting;
     private int planeCasualties;
     private int planes;
     public Dogfight(int planes) {
-        isfighting = true;
+        isFighting = true;
         this.planes = planes;
-        if (isfighting = true) {
+        if (isFighting = true) {
             System.out.println("|Battle Begin|");
         }
         System.out.println("|You Have " + planes  + " planes in the air|");
     }
     public void myPilotCasualties() {
-        isfighting = false;
+        isFighting = false;
         if (planeCasualties <= planes/2) {
             System.out.println("|Victory|");
         } else if (planeCasualties > planes/2) {
@@ -21,7 +21,12 @@ public class Dogfight extends Battle {
         System.out.println("|You lost " + planeCasualties + " planes in battle|");
     }
     public void fightEnemy() {
-        System.out.println("|Battle in progress|");
-        this.planeCasualties = (int)(Math.random() * this.planes) + 1;
+        
+        if (isFighting == true) {
+            System.out.println("|Battle in progress|");
+            this.planeCasualties = (int)(Math.random() * this.planes) + 1;
+        } else if (isFighting == false) {
+            System.out.println("|Reset to play again|");
+        }
     }
 }
